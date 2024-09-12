c1efbfcd3775754e732610c3861cfd6ecc06b5eb
motab19 <mota82589@gmail.com>
2024-09-12 17:52:16 +0300

    S

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Profile: React.FC = () => {
   return (
@@ -9,3 +9,11 @@ const Profile: React.FC = () => {
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
